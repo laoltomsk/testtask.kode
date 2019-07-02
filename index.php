61eac7330cdc37e575b61564aca4b3cc1f2916c5
DNS <DNS@DESKTOP-V3EES5J>
2019-07-02 22:21:20 +0700

    Basic architecture.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, PATCH");
+var_dump($_REQUEST);
+
+$db = mysqli_connect("localhost", "blog", "S51@akRv", "blog");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$data = json_decode(file_get_contents('php://input'));
+
+$cleanUrl = (isset($_GET['q'])) ? $_GET['q'] : '';
+$cleanUrl = rtrim($cleanUrl, '/');
+$cleanUrl = explode('/', $cleanUrl);
+
+$controller = $cleanUrl[0];
+$url = array_slice($cleanUrl, 1);
+
+include_once 'controllers/' . $controller . '.php';
+exec($method, $url, $data);
